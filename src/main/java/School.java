4bbfb7b8d7ff9164c65a29711e674649815df482
arpit goyal <arpit.goyal@BLRETV-C02YQGN3.local>
2021-06-01 17:32:25 +0530

    initial code

---
+++ src/main/java/School.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class School {
+
+
+
+    public static void showAllSmart(List<Student> students) {
+            for(Student s: students) {
+                System.out.println(s);
+            }
+            System.out.println("----------------");
+
+    }
+
+    public static void showAllSmart(List<Student> students) {
+        for(Student s: students) {
+            if( s.getGrade() > 65) {
+                System.out.println(s);
+            }
+        }
+
+        System.out.println("----------------");
+
+    }
+    public static void main(String[] args) {
+        List<Student> roster = List.of(
+                Student.of("Fred",78,"Math","Physics"),
+                Student.of("Jim",58,"Art"),
+                Student.of("Sheila",89,"Math","Physics","Astro Physics","Quantum mechanics")
+        );
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class Student {
+
+    private  int grade;
+    private String name;
+    List<String> courses;
+
+    private Student(int grade, String name, List<String> courses) {
+        this.courses = courses;
+        this.name = name;
+        this.grade = grade;
+    }
+
+
+    // LIST.OF is in java 9
+    public static Student of(String name,int grade,String ... courses) {
+        return new Student(grade,name,List.of(courses));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public List<String> getCourses() {
+        return courses;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "grade=" + grade +
+                ", name='" + name + '\'' +
+                ", courses=" + courses +
+                '}';
+    }
+}
