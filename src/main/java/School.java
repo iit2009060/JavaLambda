d4f6c4f3c70b9ac0a4a8d37f42df7468961cf8b9
arpit.goyal <goyal.arpit.91@gmail.com>
2021-06-01 17:46:03 +0530

    1. Solved the earlier design issue separation of concerns
    Issue with this design
    
    What we are doing is finding the interesting students ,adding them to list and printing it. For any interesting category we have to write this function ( > courses size  > grade size) everytime.

--- src/main/java/School.java
+++ src/main/java/School.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class School {
@@ -13,11 +14,33 @@ public class School {
         System.out.println("----------------");
 
     }
+
+    public static List<Student> getSmart(List<Student> students, int threshold) {
+        List<Student> rv = new ArrayList<>();
+        for(Student s: students) {
+            if( s.getGrade() > threshold) {
+                rv.add(s);
+            }
+        }
+        return rv;
+    }
+
+    public static List<Student> getEnthusiastic(List<Student> students, int threshold) {
+        List<Student> rv = new ArrayList<>();
+        for(Student s: students) {
+            if( s.getCourses().size() > threshold) {
+                rv.add(s);
+            }
+        }
+        return rv;
+    }
     public static void main(String[] args) {
         List<Student> roster = List.of(
                 Student.of("Fred",78,"Math","Physics"),
                 Student.of("Jim",58,"Art"),
                 Student.of("Sheila",89,"Math","Physics","Astro Physics","Quantum mechanics")
         );
+        showAllSmart( getSmart(roster,65));
+        showAllSmart(getEnthusiastic(roster,2));
     }
 }
