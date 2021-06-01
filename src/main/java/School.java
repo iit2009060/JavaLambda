0b37724406f12390573867c773689b9d4bbd2d15
arpit.goyal <goyal.arpit.91@gmail.com>
2021-06-01 20:36:38 +0530

    this does not work in lambda expression

--- src/main/java/School.java
+++ src/main/java/School.java
@@ -6,6 +6,12 @@ import java.util.List;
 @FunctionalInterface
 interface CriterionOfStudent {
     boolean test(Student student);
+
+    // not accessible inside lambda because if "this" exists
+    // it does not refer to our lambda object.q
+    default int value() {
+        return 3;
+    }
 }
 
 class SmartStudent implements CriterionOfStudent {
@@ -60,7 +66,7 @@ public class School {
         );
 
         CriterionOfStudent midRange = (Student student) -> {
-            return student.getGrade() > 65;
+            return student.getGrade() > this.value();
         };
 /// No,no ,no must be an interface context ,
    //     and interface must define EXACTLY one abstract  method
--- src/main/java/nothis/Example.java
+++ src/main/java/nothis/Example.java
@@ -6,15 +6,12 @@ interface MyFunction {
 
 public class Example {
     public static void main(String[] args) {
-        MyFunction strlen = new MyFunction() {
-            @Override
-            public int apply(String s) {
-                if("".equals(s)){
-                    return 0;
-                }
-                else return  1 + this.apply( s.substring(1));
-            }
-        };
+        // No "this" created by/for a lambda unlike anonymous inner ....
+      /*  MyFunction strlen = (String s) -> {
+            if ("".equals(s)) {
+                return 0;
+            } else return 1 + this.apply(s.substring(1));
+        };*/
         System.out.println("length of hello is " + String.valueOf(strlen.apply("hello")));
     }
 }
