53019c96bcca29d4a165de5beb3a818c55976aa7
arpit.goyal <goyal.arpit.91@gmail.com>
2021-06-12 16:42:04 +0530

    Using Streams instead of SuperIterable.

--- src/main/java/superiterable/UseSuperIterable.java
+++ src/main/java/superiterable/UseSuperIterable.java
@@ -6,11 +6,12 @@ public class UseSuperIterable {
 
     public static void main(String[] args) {
         SuperIterable<String> sis = new SuperIterable<>(List.of("Fred","Jim","Sheila"));
-        SuperIterable<Student> roster = new SuperIterable<>(List.of(
+        List<Student> rosterList = List.of(
                 Student.of("Fred",78,"Math","Physics"),
                 Student.of("Jim",58,"Art"),
                 Student.of("Sheila",89,"Math","Physics","Astro Physics","Quantum mechanics")
-        ));
+        );
+        SuperIterable<Student> roster = new SuperIterable<>(rosterList);
         roster.filter(s -> s.getGrade() > 70)
                 .map(s -> s.getName() +  "has grade " + s.getGrade())
                 .forEvery(s -> System.out.println(" > " + s.length()));
@@ -23,5 +24,19 @@ public class UseSuperIterable {
         roster.flatmap(s -> new SuperIterable<>(s.getCourses()))
                 .distinct()
                 .forEach(System.out::println );
+
+        rosterList.stream().filter(s -> s.getGrade() > 70)
+                .map(s -> s.getName() +  "has grade " + s.getGrade())
+                .forEach(s -> System.out.println(" > " + s.length()));
+
+        System.out.println("---------");
+        rosterList.stream().map(s -> s.getCourses())
+                .forEach(System.out::println );
+
+        System.out.println("---------");
+        rosterList.stream().flatMap(s -> (s.getCourses().stream()))
+                .distinct()
+                .forEach(System.out::println );
+
     }
 }
