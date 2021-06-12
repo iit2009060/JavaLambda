1cbc59e797a0a62c93d7a2250693cfd7ce2a77db
arpit.goyal <goyal.arpit.91@gmail.com>
2021-06-12 17:30:42 +0530

    Mapping intstream to object

--- src/main/java/usestreams/InfiniteExample.java
+++ src/main/java/usestreams/InfiniteExample.java
@@ -7,6 +7,7 @@ public class InfiniteExample {
 
     public static void main(String[] args) {
         IntStream.iterate(1, x -> x + 1)
+                .mapToObj(s -> "Number is  " + s)
                 .forEach(System.out::println);
     }
 }
