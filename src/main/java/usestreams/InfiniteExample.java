1feadd4aa1d87fd79369957edf4abab2123d5585
arpit.goyal <goyal.arpit.91@gmail.com>
2021-06-12 17:31:21 +0530

    limiting the stream

--- src/main/java/usestreams/InfiniteExample.java
+++ src/main/java/usestreams/InfiniteExample.java
@@ -7,6 +7,7 @@ public class InfiniteExample {
 
     public static void main(String[] args) {
         IntStream.iterate(1, x -> x + 1)
+                .limit(1000)
                 .mapToObj(s -> "Number is  " + s)
                 .forEach(System.out::println);
     }
