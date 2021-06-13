8b0d878652f38e59b5ca6a05c6c5883c431a757b
arpit.goyal <goyal.arpit.91@gmail.com>
2021-06-13 15:32:04 +0530

    Got out of memory error , when run with iterate and parallel. It unable to manage order of the data on multiple threads.It become unmanageable

--- src/main/java/mutating/AverageReduce.java
+++ src/main/java/mutating/AverageReduce.java
@@ -41,8 +41,8 @@ public class AverageReduce {
         // will execute the section and other will wait .Eventually you will not be able to attain much concurrency.
 
         // here each thread would have its own random generation
-        DoubleStream.generate(()  -> ThreadLocalRandom.current().nextDouble(-Math.PI,+ Math.PI))//
-       // DoubleStream.iterate(0.0,x  -> ThreadLocalRandom.current().nextDouble(-Math.PI,+ Math.PI))// eventually average to be zero
+       // DoubleStream.generate(()  -> ThreadLocalRandom.current().nextDouble(-Math.PI,+ Math.PI))//
+        DoubleStream.iterate(0.0,x  -> ThreadLocalRandom.current().nextDouble(-Math.PI,+ Math.PI))// eventually average to be zero
      //   .reduce ( this reduce does not exist on primitive)
                 .parallel()
                 .limit(2_000_000_000L)
