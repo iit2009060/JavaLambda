03453aaf480f60f8c5d76b69211d26e6e3baaf29
arpit.goyal <goyal.arpit.91@gmail.com>
2021-06-13 15:26:51 +0530

    Time taken using double iterate generation ,10.739069552, nearly same as of generate,testing single thread only

--- src/main/java/mutating/AverageReduce.java
+++ src/main/java/mutating/AverageReduce.java
@@ -41,7 +41,7 @@ public class AverageReduce {
         // will execute the section and other will wait .Eventually you will not be able to attain much concurrency.
 
         // here each thread would have its own random generation
-        DoubleStream.generate(() -> ThreadLocalRandom.current().nextDouble(-Math.PI,+ Math.PI))// eventually average to be zero
+        DoubleStream.iterate(0.0,x  -> ThreadLocalRandom.current().nextDouble(-Math.PI,+ Math.PI))// eventually average to be zero
      //   .reduce ( this reduce does not exist on primitive)
                // .parallel()
                 .limit(2_000_000_000L)
